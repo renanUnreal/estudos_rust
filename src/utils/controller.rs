30110f630b33e4ca62944d1030b189d24874a21a
renanunreal <renanunreal@gmail.com>
2024-04-29 22:51:38 -0300

    funções menu e cadastro de clientes interface e controller

--- src/utils/controller.rs
+++ src/utils/controller.rs
@@ -1,20 +1,110 @@
-use std::io;
 use super::interface as ui;
+use std::io::{self, Write};
 
-
-pub fn nav_main_menu(){
-    ui::main_menu();
-    print!("Aguardando...\n");
+fn tratar_input() -> char {
     let mut input: String = String::new();
-    io::stdin().read_line(&mut input).expect("Erro ao ler os dados");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Erro ao ler os dados");
 
-    let opc : char = match input.trim().chars().next(){
-        Some(c)=> c,
-        None=>{
+    let opc: char = match input.trim().chars().next() {
+        Some(c) => c,
+        None => {
             println!("Nenhum caracter inserido.");
-            return;
+            return ' ';
         }
     };
+    opc
+}
 
-    println!("digitado : {}", opc);
+fn form_cliente(){
+    ui::cadastrar_cliente();
+ 
+    println!("Por favor, preencha as informações da pessoa:");
+
+    // Solicitação das informações ao usuário
+    print!("ID: ");
+    io::stdout().flush().unwrap();
+    let mut id_input = String::new();
+    io::stdin()
+        .read_line(&mut id_input)
+        .expect("Erro ao ler a entrada");
+    
+
+    print!("CPF: ");
+    io::stdout().flush().unwrap();
+    let mut cpf = String::new();
+    io::stdin()
+        .read_line(&mut cpf)
+        .expect("Erro ao ler a entrada");
+
+    print!("Nome: ");
+    io::stdout().flush().unwrap();
+    let mut nome = String::new();
+    io::stdin()
+        .read_line(&mut nome)
+        .expect("Erro ao ler a entrada");
+
+    print!("Sobrenome: ");
+    io::stdout().flush().unwrap();
+    let mut sobrenome = String::new();
+    io::stdin()
+        .read_line(&mut sobrenome)
+        .expect("Erro ao ler a entrada");
+
+    print!("Data de nascimento (formato YYYY-MM-DD): ");
+    io::stdout().flush().unwrap();
+    let mut data_nascimento_str = String::new();
+    io::stdin()
+        .read_line(&mut data_nascimento_str)
+        .expect("Erro ao ler a entrada");
+
+    print!("CEP: ");
+    io::stdout().flush().unwrap();
+    let mut cep = String::new();
+    io::stdin()
+        .read_line(&mut cep)
+        .expect("Erro ao ler a entrada");
+
+    print!("Telefone: ");
+    io::stdout().flush().unwrap();
+    let mut telefone = String::new();
+    io::stdin()
+        .read_line(&mut telefone)
+        .expect("Erro ao ler a entrada");
+
+    print!("Estado civil: ");
+    io::stdout().flush().unwrap();
+    let mut estado_civil = String::new();
+    io::stdin()
+        .read_line(&mut estado_civil)
+        .expect("Erro ao ler a entrada");
+
+    println!("\n\nCliente de cpf {} cadastrado com sucesso", cpf);
+}
+
+pub fn nav_main_menu() {
+    ui::main_menu();
+    print!("Aguardando...\n");
+
+    match tratar_input() {
+        '1' => form_cliente(),
+        '2' => println!("Você escolheu a opção 2."),
+        '3' => println!("Você escolheu a opção 3."),
+        _ => {
+            println!("Opção invalida, deseja tentar novamente? (s)=sim ou (n)=não");
+            print!("Aguardando...\n");
+            let opc2: char = tratar_input();
+            if opc2 == 's' || opc2 == 'S' {
+                nav_main_menu();
+            } else {
+                println!(
+                    "Você precisa tentar novamente ou digitar uma opção válida para contnuar..."
+                );
+                println!("Digite algo para tentar novamente!");
+                tratar_input();
+                nav_main_menu();
+            }
+        }
+    }
 }
--- src/utils/interface.rs
+++ src/utils/interface.rs
@@ -7,23 +7,45 @@ fn clear_screen(){
     stdout.flush().unwrap();
 }
 
-pub fn main_menu(){
-    clear_screen();
-    
-    let header = "--- Menu Inicial ---";
-    let escolha = "Escolha a opção desejada:";
+fn header(h: &str, e : &str) {
     let separator = "#".repeat(50); // Repete "#" 50 vezes
-    let opcs : [&str; 6] = ["1-Exibir todos os Clientes", "2-Procurar Cliente", "3-Cadastrar Cliente", "4-Alterar Cliente", "5-Deletar Cliente", "6-Sair"];
 
     // Calcula o tamanho do padding para centralizar o cabeçalho
-    let padding_size = (separator.len() - header.len()) / 2;
-    // Formata o cabeçalho com o padding diretamente
-    let formatted_header = format!("{:width$}", header, width = padding_size + header.len());
-    // Imprime o cabeçalho, a escolha e o separador
-    println!("{}\n{}\n{}\n{}\n", separator, formatted_header, escolha, separator);
-    for string in &opcs {
-        print!("\n{}", string);
-    }
-    print!("\n\n{}\n", separator);
+    let padding_size = (separator.len() - h.len()) / 2;
 
+    // Formata o cabeçalho com o padding diretamente
+    let formatted_header = format!("{:width$}", h, width = padding_size + h.len());
+
+    // Imprime o cabeçalho, a escolha e o separador
+    println!("{}\n{}\n{}\n{}\n", separator, formatted_header, e, separator);
+
+
+}
+
+fn opcoes_menu(opcs: &[&str]){
+    
+    let separator = "#".repeat(50); // Repete "#" 50 vezes
+    // Imprime as opções
+    for string in opcs {
+        println!("{}", string);
+    }
+
+    // Imprime o separador final
+    println!("\n{}\n", separator);
+}
+
+pub fn main_menu(){
+    clear_screen();
+    let msg: &str = "--- Menu Inicial ---";
+    let e : &str = "Digite a opção desejada:";
+    let _opcs : [&str; 6] = ["1-Exibir todos os Clientes", "2-Procurar Cliente", "3-Cadastrar Cliente", "4-Alterar Cliente", "5-Deletar Cliente", "6-Sair"];
+    header(&msg, &e);
+    opcoes_menu(&_opcs);
+}
+
+pub fn cadastrar_cliente(){
+    clear_screen();
+    let msg = "--- Cadastrar Cliente ---";
+    let e : &str = "Preencha o campo a baixo";
+    header(&msg, &e);
 }
