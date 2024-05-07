38b97eaba9c4b780c35b497c54ca36d8fd906b6c
renanunreal <renanunreal@gmail.com>
2024-05-07 07:24:27 -0300

    Correction

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod repositories;
+mod models;
 use repositories::visitante_repositories::select_from_table;
 fn main() {
    // Chama a função para executar a consulta SELECT
-    match select_from_table() {
-        Ok(_) => println!("Consulta executada com sucesso!"),
-        Err(err) => eprintln!("Erro ao executar a consulta: {}", err),
-    }
+   match select_from_table() {
+       Ok(_) => println!("Consulta executada com sucesso!"),
+    Err(err) => eprintln!("Erro ao executar a consulta: {}", err),
+ }
+
 }
--- src/repositories/visitante_repositories.rs
+++ src/repositories/visitante_repositories.rs
@@ -4,23 +4,26 @@ use super::conn::conn;
 pub fn select_from_table() -> Result<(), postgres::Error> {
     // Estabelece a conexão com o banco de dados
     let mut client = conn()?; // Adicione 'mut' aqui
-// Executa a consulta SELECT na tabela desejada
-for row in client.query("SELECT * FROM visitantes", &[])? {
-    // Processa cada linha retornada pela consulta
-    let visitante = Visitante::new(
-        row.get(1),
-        row.get(2),
-        row.get(3),
-        row.get(4),
-        row.get(5),
-        row.get(6),
-        row.get(7),
-        row.get(8),
-    );
-    // Faça o que for necessário com os dados obtidos
-    println!("\n\n cpf: {}\n email: {}\n Nome completo: {}\n telefone: {}\n empresa: {}\n cargo: {}\n acesso: {}\n pontos: {}\n\n", 
-            visitante.cpf(), visitante.email(), visitante.nome_completo(), visitante.telefone(), visitante.empresa(),visitante.cargo(), visitante.acesso(), visitante.pontos());
-}
+    
+    // Executa a consulta SELECT na tabela desejada
+    for row in client.query("SELECT * FROM visitantes", &[])? {
+        // Processa cada linha retornada pela consulta
+        let visitante = Visitante::new(
+            row.get(1),
+            row.get(2),
+            row.get(3),
+            row.get(4),
+            row.get(5),
+            row.get(6),
+            row.get(7),
+            row.get(8),
+        );
+        // Faça o que for necessário com os dados obtidos
+        println!("\n\n cpf: {}\n email: {}\n Nome completo: {}\n telefone: {}\n empresa: {}\n cargo: {}\n acesso: {}\n pontos: {}\n\n", 
+                visitante.cpf(), visitante.email(), visitante.nome_completo(), visitante.telefone(), visitante.empresa(),visitante.cargo(), visitante.acesso(), visitante.pontos());
+    }
+    
     // Retorna Ok(()) se a operação for bem-sucedida
     Ok(())
 }
+
